8e41130bb8e1c6ff9c44890b6469bcae0464bf51
ElijahButers <Elijahbuters@gmail.com>
2016-06-20 19:04:41 +0300

    Added code to GroceryRowController that set attribute settings for NSAttributedString and strikes through the text and makes it a light gray color.

--- Groceries WatchKit Extension/GroceryRowController.swift
+++ Groceries WatchKit Extension/GroceryRowController.swift
@@ -13,4 +13,12 @@ class GroceryRowController: NSObject {
 
     @IBOutlet var textLabel: WKInterfaceLabel!
     @IBOutlet var measurementLabel: WKInterfaceLabel!
+    
+    private var cellTextAttributes: [String: AnyObject] {
+        return [NSFontAttributeName: UIFont.systemFontOfSize(16), NSForegroundColorAttributeName: UIColor.whiteColor()]
+    }
+    
+    private var strikethroughCellTextAttributes: [String: AnyObject] {
+        return [NSFontAttributeName: UIFont.systemFontOfSize(16), NSForegroundColorAttributeName: UIColor.lightGrayColor(), NSStrikethroughStyleAttributeName: NSUnderlineStyle.StyleSingle.rawValue]
+    }
 }
